33304660ef376742e37e1c404c55670d499a1918
shawnmcdowell <shawnmc@microsoft.com>
2014-12-09 14:45:32 -0800

    Initial add

---
+++ Ownership/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using System.Web.Mvc;
+
+namespace Ownership.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
